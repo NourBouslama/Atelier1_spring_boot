4b61eaddac1945598b0134180c1bb358f94330fc
NourBouslama <80712992+NourBouslama@users.noreply.github.com>
2021-04-07 22:17:56 +0200

    Add files via upload

---
+++ produits/src/test/java/com/nour/demo/ProduitsApplicationTests.java
@@ -0,0 +1,25 @@
+package com.nour.demo;
+
+
+import java.util.Date;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import com.nour.produits.entities.Produit;
+import com.nour.produits.repos.ProduitRepository;
+
+
+
+
+@SpringBootTest
+class ProduitsApplicationTests {
+@Autowired
+private ProduitRepository produitRepository;
+@Test
+public void testCreateProduit() {
+Produit prod = new Produit("PC Dell",2200.500,new Date());
+produitRepository.save(prod);
+}
+
+}
